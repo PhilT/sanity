96971765f9088a88a0a8a64bb41c74e7be5e6ed7
Phil Thompson (Home) <phil@electricvisions.com>
2010-02-03 03:23:05 +0000

    tidying up output

--- lib/build.rb
+++ lib/build.rb
@@ -3,13 +3,11 @@ class Build < Struct.new(:working_dir, :branch)
   def run
     return unless run?
     log "Starting build at #{Time.now.to_s}..."
+    cucumber = File.exists?('features') ? 'cucumber' : ''
     commands = [
-      'gems:install RAILS_ENV=test',
-      'db:migrate',
-      'db:test:prepare',
-      'default'
+      'rake gems:install RAILS_ENV=test',
+      'rake db:migrate db:test:prepare default #{cucumber}'
     ]
-    commands << 'cucumber' if File.exists?('features')
     success = false
     commands.each do |cmd|
       success = rake(cmd)
@@ -28,19 +26,16 @@ private
   end
 
   def rake(cmd)
-    cmd = "cd #{working_dir} && rake #{cmd} 2>&1"
+    cmd = "cd #{working_dir} && #{cmd} 2>&1"
     log cmd
     output = `#{cmd}`
     exitstatus = $?.exitstatus
     log output, 'RAKE'
-    log "exitstatus=#{exitstatus}"
     exitstatus == 0
   end
 
   def log(output, cmd = nil)
-    puts "[#{cmd}]" if cmd
-    puts "#{output}"
-    puts "[#{cmd}] end." if cmd
+    puts "[#{cmd}]#{output}"
   end
 end
 
