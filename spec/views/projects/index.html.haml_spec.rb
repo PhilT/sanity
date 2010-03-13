41e4eff0507b16d981c21b68182fae3006171af3
Phil Thompson <phil@electricvisions.com>
2010-03-13 23:21:26 +0000

    minor styling

--- spec/views/projects/index.html.haml_spec.rb
+++ spec/views/projects/index.html.haml_spec.rb
@@ -4,7 +4,7 @@ describe 'projects/index.html.haml' do
   include ProjectsHelper
 
   it do
-    assigns[:projects] = [mock_model(Project, :name => 'A Project', :builds => [mock_model(Build, :created_at => Time.now, :success? => true)])]
+    assigns[:projects] = [mock_model(Project, :name => 'A Project', :builds => [mock_model(Build, :created_at => Time.now, :success? => true)], :clone_from => 'git clone URL')]
     render
     response.body.should be_valid_xhtml
   end
