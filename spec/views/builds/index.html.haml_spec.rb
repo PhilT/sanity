6daa7f29e84c0f98875962e732ab2b02c8cda1fd
Phil Thompson <phil@electricvisions.com>
2010-03-16 21:42:26 +0000

    fixed tests

--- spec/views/builds/index.html.haml_spec.rb
+++ spec/views/builds/index.html.haml_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'builds/index.html.haml' do
 
-  it do
+  it 'should be valid XHTML' do
     assigns[:project] = mock_model(Project, :name => 'A Project', :clone_from => 'git clone URL')
-    assigns[:builds] = [mock_model(Build, :created_at => Time.now, :updated_at => 'some time ago', :success? => true, :commit_hash => '1234567', :committed_at => 'some time', :branch => 'master', :author => 'me', :commit_message => 'some message')]
+    assigns[:builds] = [mock_model(Build, :created_at => Time.now, :updated_at => 'some time ago', :success? => true, :commit_hash => '1234567', :committed_at => 'some time', :branch => 'master', :author => 'me', :commit_message => 'some message', :completed_at => 'some time')]
     render
     response.body.should be_valid_xhtml
   end
--- spec/views/projects/index.html.haml_spec.rb
+++ spec/views/projects/index.html.haml_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'projects/index.html.haml' do
   include ProjectsHelper
 
-  it do
-    assigns[:projects] = [mock_model(Project, :name => 'A Project', :builds => [mock_model(Build, :created_at => Time.now, :success? => true)], :clone_from => 'git clone URL')]
+  it 'should be valid XHTML' do
+    assigns[:projects] = [mock_model(Project, :name => 'A Project', :builds => [mock_model(Build, :created_at => Time.now, :success? => true, :completed_at => 'some time')], :clone_from => 'git clone URL')]
     render
     response.body.should be_valid_xhtml
   end
