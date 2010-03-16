ac0af487b644230f9d3d899a9ace361ea41e0184
Phil Thompson (Home) <phil@electricvisions.com>
2010-03-16 20:58:52 +0000

    Added build running ui state.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,23 @@
 module ProjectsHelper
   def build_class(build)
-    build.try(:success?) ? 'build_successful' : 'build_failed'
+    return 'build_notrun' if build.nil?
+    if build.completed_at.nil?
+      'build_running'
+    elsif build.success?
+      'build_successful'
+    else
+      'build_failed'
+    end
+  end
+
+  def status(build)
+    {'build_running' => 'running', 'build_successful' => 'built successfully', 'build_failed' => 'failed'}[build_class(build)]
   end
 
   def last_build_status(build)
-    status = build.try(:success?) ? 'successfully' : 'with errors'
-    build.nil? ? "not built yet" : "Last build ran #{status} #{time_ago_in_words(build.created_at)} ago"
+    return 'Build never run' if build.nil?
+    status = {'build_running' => 'Build started', 'build_successful' => 'Last build ran successfully', 'build_failed' => 'Last build failed'}[build_class(build)]
+    "#{status} #{time_ago_in_words(build.created_at)} ago"
   end
 end
 
--- public/javascripts/builds.js
+++ public/javascripts/builds.js
@@ -1,6 +1,6 @@
 $(function(){
   last_build = $('#builds a').attr('href');
   last_build = last_build.substr(last_build.lastIndexOf('/') + 1);
-  setInterval("$.get(document.URL + '.js', null, function(data, status){$('#content').html(data)}, 'html')", 60000);
+  setInterval("$.get(document.URL + '.js?since=' + last_build, null, function(data, status){$('#content').html(data)}, 'html')", 60000);
 })
 
