4eb7adce741279962fa4032eacdf0ca62525b6bb
Phil Thompson (Home) <phil@electricvisions.com>
2010-03-19 16:38:32 +0000

    Made new project form wider. Added prompts. Fixed bug causing page reload when on pages other than projects index

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -3,7 +3,7 @@ $(function(){
     $.get(document.URL + '.js', null, function(data, status){$('#content').html(data)}, 'html');
   }
 
-  if($('#projects')){
+  if($('#projects')[0]){
     setInterval(updateProjects, 60000);
   }
 })
